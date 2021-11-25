52473ebefb23bc4f3fa2fb60b5262de0edc08a67
Over441 <dessart.loo@orange.fr>
2021-11-25 18:02:22 +0100

    Update index.php

--- Site/index.php
+++ Site/index.php
@@ -26,5 +26,7 @@ if(isset($_POST['valider'])){
         <input type="submit" name="valider">
 </form>
 <section id="messages"></section>
+<br>
+<p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><span property="dct:title">Site web JOJO</span> by <span property="cc:attributionName">Joris Coutelier</span> is licensed under <a href="http://creativecommons.org/licenses/by-nc/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">Attribution-NonCommercial 4.0 International<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1"></a></p>
 </body>
 </html>
