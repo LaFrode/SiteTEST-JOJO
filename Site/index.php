23f923bcac238ba6f6eea8363d1bee8dd4c2476d
Over441 <dessart.loo@orange.fr>
2021-11-25 12:05:27 +0100

    PHP

---
+++ Site/index.php
@@ -0,0 +1,22 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Site</title>
+</head>
+<body>
+    <div id="carte">
+        <h1>POTOBOX</h1>
+    </div>
+    <from method="POST" action="" align="center">
+        <input type="text" name="pseudo">
+        <br><br>
+        <textarea name="message"></textarea>
+        <br>
+        <input type="submit" name="valider">
+</form>
+<section id="messages"></section>
+</body>
+</html>
